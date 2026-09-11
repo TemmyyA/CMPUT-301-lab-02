b9dd52216792c39782c0dba39a7f3b0eeeb3b70a
TemmyyA <oalamu@ualberta.ca>
2026-09-10 18:32:34 -0600

    Completed Lab

---
+++ ListyCity/app/src/main/java/com/example/listcity/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.listcity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.listcity.ui.theme.ListCityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val cityRepository = CityRepository()
+        setContent {
+            ListCityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CityListScreen(
+                        cities = cityRepository.cities,
+                        onAddCity = { cityRepository.addCity(it) },
+                        onDeleteCity = { cityRepository.deleteCity(it) },
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CityListScreen(
+    cities: List<String>,
+    onAddCity: (String) -> Unit,
+    onDeleteCity: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var newCityName by remember { mutableStateOf(value = "") }
+    var selectedCity by remember { mutableStateOf<String>("") }
+
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(modifier = Modifier.padding(all = 16.dp)) {
+            OutlinedTextField(
+                value = newCityName,
+                onValueChange = { newCityName = it },
+                label = { Text("City name") },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = {
+                    if (newCityName.isNotBlank()) {
+                        onAddCity(newCityName)
+                        newCityName = ""
+                    }
+                }
+            ) {
+                Text("Add City")
+            }
+
+            Button(
+                onClick = {
+                    onDeleteCity(selectedCity)
+                }
+            ) {
+                Text("Delete City")
+            }
+
+        }
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(cities) { city ->
+                CityRow(city = city, onClick = { selectedCity = city })
+            }
+        }
+    }
+}
+
+@Composable
+fun CityRow(city: String, onClick: () -> Unit) {
+    Text(
+        text = city,
+        fontSize = 28.sp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(horizontal = 18.dp, vertical = 14.dp)
+    )
+}
+
+class CityRepository {
+    // Keep mutable app data private
+    private val _cities = mutableStateListOf(
+        "Edmonton", "Vancouver", "Moscow",
+        "Sydney", "Berlin", "Vienna",
+        "Tokyo", "Beijing", "Osaka",
+        "New Dehli"
+    )
+
+    // Read-only list for UI
+    val cities: List<String>
+        get() = _cities
+
+    fun addCity(city: String) {
+        _cities.add(city)
+    }
+
+    fun deleteCity(city: String) {
+        _cities.remove(city)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ListCityTheme {
+        Greeting("Android")
+    }
+}
